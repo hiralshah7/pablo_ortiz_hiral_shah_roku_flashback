d2cfdec3d09f190917760f1078b0b053516878f6
Hiral <90278193+hiralshah7@users.noreply.github.com>
2023-04-07 03:06:47 -0400

    Update TheKidsHomeComponent.js

--- roku_frontend/public/js/components/TheKidsHomeComponent.js
+++ roku_frontend/public/js/components/TheKidsHomeComponent.js
@@ -3,25 +3,48 @@ export default {
 
     template: `
 
-    <div>
-    <h1>This is the Kid's homepage</h1>
-    <p>This is where the kids can watch their favourite movies and tv shows</p>
-    <ul class="moviekid">
-      <li v-for="movie in movies" :key="movie.id">
-        <img :src="movie.image" :alt="movie.title" @click="playVideo(movie)">
-        <h3>{{ movie.title }}</h3>
-        <p>Year: {{ movie.year }}</p>
-        <p>Rating: {{ movie.imDbRating }}</p>
-      </li>
-    </ul>
-    <div v-show="showVideo" class="video-container">
-      <video :src="selectedMovie.video" controls autoplay></video>
-      <h2>{{ selectedMovie.title }}</h2>
-      <p>Year: {{ selectedMovie.year }}</p>
-      <p>Rating: {{ selectedMovie.imDbRating }}</p>
-      <button @click="showVideo = false">Close</button>
-    </div>
-  </div>
+
+<!-- movies for kids -->
+
+   <!--  creating three button one for music, one for movies and one for tv shows -->
+   <!-- load the content of the button when the user clicks on it --> 
+        <div class="main_entertainment">
+          <button class="btn_entertainment" @click="loadKidsMovies">Movies</button>
+          <button class="btn_entertainment" @click="loadKidsMusic">Music</button>
+          <button class="btn_entertainment" @click="loadKidsTv">Tv Shows</button>
+        </div>
+
+    <section class="movie_container" v-show="showVideo1">
+      <ul class="moviekid">
+        <li v-for="movie in movies" :key="movie.id">
+          <img :src="movie.image" :alt="movie.title" @click="playVideo(movie)">
+          <h3>{{ movie.title }}</h3>
+          <p>Year: {{ movie.year }}</p>
+          <p>Rating: {{ movie.imDbRating }}</p>
+        </li>
+      </ul>
+      <!-- open the video when the user clicks on the image -->
+      <div class="video_container" v-show="showVideo">
+        <video :src="selectedMovie.video" controls autoplay></video>
+        <h2>{{ selectedMovie.title }}</h2>
+              <p>Year: {{ selectedMovie.year }}</p>
+              <p>Rating: {{ selectedMovie.imDbRating }}</p>
+        <button @click="showVideo = false">Close</button>
+      </div>
+  </section>
+
+  <!-- music for kids -->
+  <section v-show="showMusic" class="music_container">
+      <h2 class="music_title">Music</h2>
+  </section>
+
+
+  <!-- tv shows for kids -->
+  <section v-show="showTv" class="tv_container">
+      <h2 class="tv_title">Tv Shows</h2>
+  </section>
+
+
 
 
   
@@ -32,52 +55,83 @@ export default {
         return {
           movies: [],
           showVideo: false,
-          selectedMovie: {}
+          selectedMovie: {},
+          selectedMusic: {},
+          selectedTvshow: {},
+          showMusic: false,
+          showTv: false,
+          showVideo1: true,
+
+
+
         };
 
         
 
     },
 
-    // write js code here
-
-    // create a method that will fetch the data from the api
-
-  
     created() {
-        const options = {
-            method: 'GET',
-            headers: {
-              'X-RapidAPI-Key': '6325459dcfmsh01df99951fe2399p15d8ccjsndfd3ff8f51b2',
-              'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-            }
-          };
-      
-          var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-      
-          fetch('https://imdb-api.com/en/API/Keyword/k_484b899g/children', requestOptions)
-            .then(response => response.json())
-            .then(result => {
-              this.movies = result.items;
-            })
-            .catch(error => console.log('error', error));
-
-            
-        },
-
-        methods: {
-            playVideo(movie) {
-                // set the selected movie and show the video container
-                this.selectedMovie = movie;
-                this.showVideo = true;
-              }
-
-
-              
-            }
-          
+      // Fetching data for Kids Movies
+      const movieOptions = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "6325459dcfmsh01df99951fe2399p15d8ccjsndfd3ff8f51b2",
+          "X-RapidAPI-Host": "imdb-api.com"
         }
-        
+      };
+  
+      fetch(
+        "https://imdb-api.com/en/API/Keyword/k_484b899g/children",
+        movieOptions
+      )
+        .then(response => response.json())
+        .then(result => {
+          this.movies = result.items;
+        })
+        .catch(error => console.log("error", error));
+  
+    },
+
+    // fetch data for kids music
+
+
+    // fetch data for kids tv shows
+
+
+
+    methods: {
+      playVideo(movie) {
+        this.selectedMovie = movie;
+        this.showVideo = true;
+        console.log ( "video box is open ");
+      },
+      playMusic(music) {
+        this.selectedMusic = music;
+        this.showMusic = true;
+        console.log ( "music box is open ");
+      },
+
+      loadKidsMovies() {
+        this.showVideo1 = true;
+        this.showMusic = false;
+        this.showTv = false;
+        console.log ( "movies are loaded ");
+      },
+
+      loadKidsMusic() {
+        this.showVideo1 = false;
+        this.showMusic = true;
+        this.showTv = false;
+
+        console.log ( "music is loaded ");
+      },
+
+      loadKidsTv() {
+        this.showVideo1 = false;
+        this.showMusic = false;
+        this.showTv = true;
+        console.log ( "tv shows are loaded ");
+      }
+
+    }
+  };
